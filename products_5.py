bdd5e7d5e3d171fc49232037f90d085d395a5ade
Jay Yu <jayyu231@gmail.com>
2021-02-11 16:05:02 +0800

    refactor

---
+++ products_5.py
@@ -0,0 +1,51 @@
+import os
+#讀取檔案 
+def read_file(filename):
+    products =[]
+    with open (filename, 'r') as f:
+        for line in f:
+            if '商品, 價格' in line:
+                continue #繼續下一回,跳掉下面兩行
+            name, price= line.strip().split(',')
+            products.append([name, price])
+    return products
+
+#讓使用者輸入
+def user_input(products):
+    while True:
+        name = input('請輸入商品名稱: 跳出請按q ')
+        if name == 'q': 
+            break
+        price = input('請輸入商品價格: ')
+        price = int(price)
+        products.append([name, price]) #3 
+    print(products)
+    return products
+#印所有購買紀錄
+def print_products(products):
+    for p in products:
+        print(p[0], '的價格是', p[1])
+
+#寫入檔案
+def write_file(filename, products):
+    with open (filename, 'w') as f:
+            f.write('商品, 價格\n')
+        
+            for p in products:
+                f.write(p[0] + ',' + str(p[1]) + '\n')
+
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename): #先確認檔案在不在?
+        print('找到檔案了')
+        products = read_file(filename)
+    else:
+        print('找不到檔案')
+
+    products = user_input(products)
+    print_products(products)
+    write_file('products.csv', products)
+
+main()
+
+#refactor 重構
